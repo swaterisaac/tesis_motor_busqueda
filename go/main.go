89fe940ad981b4aa5e36ec4260799e9aaaed6d45
Isaac <isaac.rojas@usach.cl>
2021-12-02 23:02:13 -0300

    ADD - CORS para gin

--- go/main.go
+++ go/main.go
@@ -5,6 +5,7 @@ import (
 	"tesis/conexion"
 
 	elasticsearch "github.com/elastic/go-elasticsearch/v6"
+	cors "github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
@@ -37,6 +38,16 @@ func main() {
 	}
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+	}))
 	router.GET("/ofertas", func(c *gin.Context) {
 		ofertas, err := busquedas.ObtenerOfertasRecomendacion(db, es, 12)
 		if err != nil {
